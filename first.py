38af531f3baa9457c948deea8d3e0d9133658bbd
rohitpakhre <rohitpakhrekh@gmail.com>
2021-08-09 10:33:38 +0530

    start coding

---
+++ first.py
@@ -0,0 +1,9 @@
+class xyx:
+    def print_ajitnal(x):
+        print(x)
+    def print_sikandra(x):
+        print(x)
+
+def __main__ == "__name__":
+    obj = xyx()
+    obj.print_ajitnal("Vishal chak")
